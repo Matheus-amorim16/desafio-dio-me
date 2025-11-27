20ce44658cd8e946eaeed2ab53fccbb46b45e273
Matheus-amorim16 <matheuz16@outlook.com>
2025-11-27 01:58:49 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let nivel = 5000
+let nomeHeroi = "Zakarim"
+
+
+    if (nivel <=  1000){
+    console.log(" Ao que parece o herói:  "+ nomeHeroi + " pertence ao Rank Ferro! "); 
+    }    else if ((nivel >= 1001) && (nivel <= 2000)){
+       console.log (" Ao que parece o herói: "+nomeHeroi+" pertence ao Rank Bronze!")
+    }    else if ((nivel >= 2001) && (nivel <= 5000)){
+       console.log ("Ao que parece o herói: "+nomeHeroi+" pertence ao rank Prata!");}
+    else if ((nivel >= 5001) && (nivel<= 7000 )){
+       console.log ("Ao que parece o herói: "+nomeHeroi+" pertence ao rank Ouro");}
+    else if ((nivel >= 7001) && (nivel <= 8000)){
+        console.log(" Ao que parece o  héroi: "+nomeHeroi+ " pertence ao Rank Platina!")}
+    else if ((nivel >= 8001) && (nivel <= 9000)){  
+        console.log ("Ao que parece o herói: "+nomeHeroi+" pertence ao Rank Ascendente!!");}
+    else if ((nivel >= 9001) && (nivel<= 10000)){
+        console.log(" Ao que parece o herói:"+nomeHeroi+ " pertence ao Rank IMORTAL!!!!");}
+    else if (nivel >= 10001){
+        console.log(" Ao que parece o incrível Herói: "+nomeHeroi+" pertence ao Rank RADIANTE!!!! Parabéns, você é incrível!");}
+    
